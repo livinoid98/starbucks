e175e405a4d9414d5e7736823a6af1e9fdb73644
livinoid98 <ccy9803@naver.com>
2020-02-27 15:09:14 +0900

    Updates

---
+++ precache-manifest.89de19b1f4cc406bd47d4783d8752c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8158d63a3bf3ee32ddefe09d746ff7a1",
+    "url": "/starbucks/index.html"
+  },
+  {
+    "revision": "583ba72bcbd57703ac24",
+    "url": "/starbucks/static/css/main.66bfcb23.chunk.css"
+  },
+  {
+    "revision": "214760909eb8f7a1da78",
+    "url": "/starbucks/static/js/2.fe900b72.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/starbucks/static/js/2.fe900b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583ba72bcbd57703ac24",
+    "url": "/starbucks/static/js/main.670bd6b5.chunk.js"
+  },
+  {
+    "revision": "76c7b7ad621affa84b5f",
+    "url": "/starbucks/static/js/runtime-main.78174637.js"
+  }
+]);
